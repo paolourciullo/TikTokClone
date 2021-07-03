cd96fcd801a4e9b51d40146af80bee42975b2854
Paolo U <paolourciullo@gmail.com>
2021-07-03 11:16:56 -0400

    add routes to index.js/17:00

--- tiktokclone/src/index.js
+++ tiktokclone/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
 
 import './index.css'
-import App from './App'
+
+const App = () => {
+  return
+  ;<HashRouter>
+    <Switch>
+      <Route path="/" component={home} />
+      <Route path="/upload" component={Upload} />
+    </Switch>
+  </HashRouter>
+}
+
+export default App
 
 ReactDOM.render(
   <React.StrictMode>
