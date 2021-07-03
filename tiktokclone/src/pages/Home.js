b73714b907087432f9b99d96a116f0e50b956020
Paolo U <paolourciullo@gmail.com>
2021-07-03 13:21:22 -0400

    style,fc,home/27:41

--- tiktokclone/src/pages/Home.js
+++ tiktokclone/src/pages/Home.js
@@ -1,7 +1,13 @@
+import FollowersColumn from '../components/FollowersColumn'
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <FollowersColumn />
+      <div className="feed">
+        <h1>Home</h1>
+      </div>
+      <div className="suggested-box"></div>
     </div>
   )
 }
